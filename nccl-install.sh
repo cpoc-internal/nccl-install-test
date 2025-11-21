433b434507c5b06781f5e530c4519129aa30a8b4
earguedast <este.at@gmail.com>
2025-11-21 15:28:21 -0600

    Update nccl-install.sh

--- nccl-install.sh
+++ nccl-install.sh
@@ -7,5 +7,12 @@ sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 
 sudo apt install libnccl2=2.28.7-1+cuda12.9 libnccl-dev=2.28.7-1+cuda12.9 -y
+sudo apt install build-essential devscripts debhelper fakeroot -y
+make pkg.debian.build
+ls build/pkg/deb/
+echo "cloning nccl test"
+git clone https://github.com/NVIDIA/nccl-tests.git
+cd nccl-tests
+./build/all_reduce_perf -b 8 -e 256M -f 2 -g 2
 
 echo "done"
